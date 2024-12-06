7e1b7f3838d433ca51810345dc34606efa01d3a2
chenab0 <chenabx1@gmail.com>
2024-12-06 15:47:53 -0800

    Update code.py

--- hw6/code.py
+++ hw6/code.py
@@ -35,7 +35,7 @@ u = np.tanh(r) * np.cos(m * theta - r)
 v = np.tanh(r) * np.sin(m * theta - r)
 u_cap = fft2(u)
 v_cap = fft2(v)
-y_flat = np.concatenate([u_cap.flatten(), v_cap.flatten()])
+y_flat = np.hstack([u_cap.flatten(), v_cap.flatten()])
 
 # Define spectral k values
 kx = (2 * np.pi / Lx) * np.concatenate((np.arange(0, nx/2), np.arange(-nx/2, 0)))
@@ -49,34 +49,32 @@ K = KX**2 + KY**2
 #FFT SPECTRAL METHOD
 def spc_rhs(t, y_flat):
   # Split the 1D array back into u and v components
-  n_half = len(y_flat)// 2
-  u_cap_flat = y_flat[:n_half]
-  v_cap_flat = y_flat[n_half:]
+  u_cap_flat = y_flat[:n**2]
+  v_cap_flat = y_flat[n**2:]
 
   #Reshape to 2D
   u_cap = u_cap_flat.reshape(nx, ny)
   v_cap = v_cap_flat.reshape(nx, ny)
 
-  u = np.real(ifft2(u_cap))
-  v = np.real(ifft2(v_cap))
+  u = ifft2(u_cap)
+  v = ifft2(v_cap)
 
   A2 = u**2 + v**2
   lamda = 1 - A2
-  omega = -1*beta*A2
+  omega = -beta*A2
 
   f = lamda * u - omega * v
   g = omega * u + lamda * v
 
-  u_t = fft2(f) + D1 * K**2 * u_cap
-  v_t = fft2(g) + D2 * K**2 * v_cap
+  u_t = fft2(f) - D1 * K * u_cap
+  v_t = fft2(g) - D2 * K * v_cap
 
-  return np.concatenate([u_t.flatten(), v_t.flatten()])
+  return np.hstack([u_t.flatten(), v_t.flatten()])
 
 #Solve ODE
 sol_fft = solve_ivp(spc_rhs, tp, y_flat, t_eval = tspan, args=(), method='RK45')
 A1 = sol_fft.y
-print(A1.shape)
-print(A1)
+
 
 #CHEBYCHEV METHOD
 def cheb(N):
@@ -101,17 +99,17 @@ D, x_cheb = cheb(n)
 
 #No flux boundaries
 D[0, :] = 0
-D[-1, :] = 0
-
+D[n, :] = 0
+D2 = ((2/L)**2)*np.dot(D, D)
 #Making Grid
 x_cheb = (L/2) * x_cheb #Rescaling to [-10, 10]
-y_cheb = (L/2) * x_cheb #Same for Y
+y_cheb =  x_cheb #Same for Y
 X, Y = np.meshgrid(x_cheb, y_cheb)
 
 #Computing Laplacian
-D2 = D @ D
+
 I = np.eye(len(D2))
-Laplacian = ((2/L)**2)*(np.kron(I, D2) + np.kron(D2, I)) #2D Laplacian
+Laplacian = np.kron(I, D2) + np.kron(D2, I) #2D Laplacian
 
 #Initial conditions
 m = 1  #number of spirals
@@ -123,7 +121,7 @@ v = np.tanh(r) * np.sin(m * theta - r)
 
 u_flat = u.flatten()
 v_flat = v.flatten()
-y_flat = np.concatenate([u_flat, v_flat])
+y_flat = np.hstack([u_flat, v_flat])
 
 # Parameters
 D1 = 0.1
@@ -141,7 +139,7 @@ def cheb_rhs(t, y_flat):
   u = u_flat.reshape(nx + 1, ny + 1)
   v = v_flat.reshape(nx + 1, ny + 1)
 
-  A2 = u**2 + v**2
+  A2 = u*u + v*v
   lamda = 1 - A2
   omega = -1*beta*A2
 
